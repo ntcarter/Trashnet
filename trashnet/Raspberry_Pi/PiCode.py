bc9ce0b0c39072260843e4721a26fdae83ce1657
Sam <guenette@iastate.edu>
2018-03-25 19:06:59 -0500

    mod

--- trashnet/Raspberry_Pi/PiCode.py
+++ trashnet/Raspberry_Pi/PiCode.py
@@ -4,26 +4,25 @@ import sqlite3
 import MySQLdb as mdb
 
 #!/usr/bin/env python
-
 IO.setwarnings(False)
 IO.setmode(IO.BCM)
 IO.setup(14,IO.IN) #GPIO 14 -> IR sensor as input
 
 conn = mdb.Connection(db='NAME OF DATABASE', host='localhost', user='root', passwd='PASSWORD')
-cur = conn.cursor()
+db = conn.cursor()
 id = 1;
-
-
+full = false
 
 def monitor:
 	if(IO.input(14)==False): #object is near
 		#put replace with update SQL command
-		cur.execute("UPDATE visit SET time =" + time.time() + ", ")
+		db.execute("UPDATE visit SET time =" + time.time() + ", ")
 		print ("trash thrown out")
-		time.sleep(5)
-		if(IO.input == False):
+		time.sleep(0.5)
+		
+		if(IO.input(14) == False):
 			print("trash is full")
-			cur.execute("UPDATE alerts SET full =" + true + ". where Id = " + id)
+			db.execute("UPDATE alerts SET full =" + true + ". where Id = " + id + "time = " + time.time())
 			
 while 1:
 	monitor
