07d96f94860afebf76df8d744b209a03ef30d85e
bmchodur <bmchodur@iastate.edu>
2018-04-04 20:33:38 -0500

    EventLog.php for event log table in the stats.php

---
+++ trashnet/site/phpcode/EventLog.php
@@ -0,0 +1,17 @@
+<?php
+
+// // getting data from database
+$conn = mysqli_connect("trashnet.ece.iastate.edu", "logan", "ROFLdb!789", "trashnet_db");
+
+// // getting data from table
+$result = mysqli_query($conn, "SELECT * FROM trashnet_db.eventsLog");
+
+// // storing in array
+$data = array();
+while($row = mysqli_fetch_assoc($result)){
+	$data[] = $row;
+}
+
+// // returning response in JSON format
+
+echo json_encode($data);
