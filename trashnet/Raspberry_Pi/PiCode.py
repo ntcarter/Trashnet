9772efcc7b82a8bfac1e2fdb478d6a3d37099cb6
Sam <guenette@iastate.edu>
2018-04-07 15:54:51 -0500

    mod

--- trashnet/Raspberry_Pi/PiCode.py
+++ trashnet/Raspberry_Pi/PiCode.py
@@ -1,7 +1,9 @@
 ##import RPi.GPIO as IO
 import time
 import sqlite3
-import MySQLdb as mdb
+import datetime
+#Server Connection to MySQL:
+import MySQLdb
 
 ##SONAR
 import RPi.GPIO as GPIO
@@ -43,74 +45,35 @@ def distance():
 	return distance
 
 ##
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ##IO.setwarnings(False)
 ##IO.setmode(IO.BCM)
 ##IO.setup(14,IO.IN) #GPIO 14 -> IR sensor as input
 
-conn = sqlite3.Connection(db='test', host='trashnet.ece.iastate.edu', user='root', passwd='PASSWORD')
-
-db = conn.cursor()
-id = 1;
 ## 0 = empty
 ## 1 = full
 ## 3 = trash thrown 
-status = 0;
+
+conn = MySQLdb.connect(host= "trashnet.ece.iastate.edu",user="logan",passwd="ROFLdb!789",db="trashnet_db")
+cursor = conn.cursor()
+status = "empty";
+id = 1
 ##full = false
 
 def monitor():
 	if(IO.input(14)==False): #object is near
-		db.execute("UPDATE events SET location = 'East Side' where ID = " + id)
-		print ("trash thrown out")
+		db.execute("INSERT INTO eventsLog (UnitId, EventType, EventTime) VALUES (" + id + ", 3," + datetime.time() +")")
+		print ("trash thrown in")
 		time.sleep(0.5)
 		
 		if(IO.input(14) == False):
 			print("trash is full")
-			db.execute("UPDATE alerts SET full =3" + ". where Id = " + id + "time = " + time.time())
-		else
-			print("trash has been thrown in")
-			db.execute("INSERT INTO eventsLog (UnitId, EventType, EventTime) VALUES (" + id + ", 3,"  + time.time() + ")")
-
-
-
-def test():
-	print("Hello", flush=True)
-	time.sleep(1)
-	print("World", flush=True)
-	time.sleep(1)
+			trash = "full"
+			##sql update
+			try:
+   				cursor.execute("UPDATE binStatus SET Status ='Full' WHERE Id = " + id)
+   				conn.commit()
+			except:
+   				conn.rollback()
 	
 while 1:
 	monitor()
