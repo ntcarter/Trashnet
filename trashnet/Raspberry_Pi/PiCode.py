92afa7d34a23c0af772ba0d17ded6f1e672793c1
Sam <guenette@iastate.edu>
2018-04-07 13:32:01 -0500

    sonar mod

--- trashnet/Raspberry_Pi/PiCode.py
+++ trashnet/Raspberry_Pi/PiCode.py
@@ -1,28 +1,111 @@
-import RPi.GPIO as IO
+##import RPi.GPIO as IO
 import time
 import sqlite3
 import MySQLdb as mdb
 
-#!/usr/bin/env python
-IO.setwarnings(False)
-IO.setmode(IO.BCM)
-IO.setup(14,IO.IN) #GPIO 14 -> IR sensor as input
+##SONAR
+import RPi.GPIO as GPIO
 
-conn = mdb.Connection(db='NAME OF DATABASE', host='localhost', user='root', passwd='PASSWORD')
+def distance():
+
+	GPIO.setmode(GPIO.BCM)
+
+	TRIG = 23 
+	ECHO = 24
+
+	print "Distance Measurement In Progress"
+
+	GPIO.setup(TRIG,GPIO.OUT)
+	GPIO.setup(ECHO,GPIO.IN)
+
+	GPIO.output(TRIG, False)
+	print "Waiting For Sensor To Settle"
+	time.sleep(2)
+
+	GPIO.output(TRIG, True)
+	time.sleep(0.00001)
+	GPIO.output(TRIG, False)
+
+	while GPIO.input(ECHO)==0:
+  		pulse_start = time.time()
+
+	while GPIO.input(ECHO)==1:
+  		pulse_end = time.time()
+
+	pulse_duration = pulse_end - pulse_start
+
+	distance = pulse_duration * 17150
+
+	distance = round(distance, 2)
+
+	print "Distance:",distance,"cm"
+	GPIO.cleanup()
+	return distance
+
+##
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##IO.setwarnings(False)
+##IO.setmode(IO.BCM)
+##IO.setup(14,IO.IN) #GPIO 14 -> IR sensor as input
+
+conn = sqlite3.Connection(db='test', host='trashnet.ece.iastate.edu', user='root', passwd='PASSWORD')
+
 db = conn.cursor()
 id = 1;
-full = false
+## 0 = empty
+## 1 = full
+## 3 = trash thrown 
+status = 0;
+##full = false
 
-def monitor:
+def monitor():
 	if(IO.input(14)==False): #object is near
-		#put replace with update SQL command
-		db.execute("UPDATE visit SET time =" + time.time() + ", ")
+		db.execute("UPDATE events SET location = 'East Side' where ID = " + id)
 		print ("trash thrown out")
 		time.sleep(0.5)
 		
 		if(IO.input(14) == False):
 			print("trash is full")
 			db.execute("UPDATE alerts SET full =" + true + ". where Id = " + id + "time = " + time.time())
-			
+
+def test():
+	print("Hello", flush=True)
+	time.sleep(1)
+	print("World", flush=True)
+	time.sleep(1)
+	
 while 1:
-	monitor
+	monitor()
