3e035fb655796516e8095bcc77f170a86471a820
ntcarter <ntcarter@iastate.edu>
2018-04-12 15:52:19 -0500

    Upload New File

---
+++ trashnet/site/phpcode/RegDev.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<h2>Register A Device (All fields are required):</h2><br>
+<form action="Registration.php" method="post">
+
+Your company name:<br>
+ <input type="text" name="OwnerId"><br>
+Longitude:<br>
+  <input type="text" name="Longitude"><br>
+Latitude:<br>
+   <input type="text" name="Latitude"><br>
+Address:<br>
+  <input type="text" name="Address"><br>
+<input type="submit" value="submit">
+</form>
+
+</body>
+</html>
