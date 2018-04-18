9504aaaac097463309d440bc7bedbf9a8dfe5109
Sam <guenette@iastate.edu>
2018-04-18 01:57:32 -0500

    mod

--- trashnet/Raspberry_Pi/sql.py
+++ trashnet/Raspberry_Pi/sql.py
@@ -1,12 +1,15 @@
 import datetime
 #Server Connection to MySQL:
-
 import MySQLdb
+
 conn = MySQLdb.connect(host= "trashnet.ece.iastate.edu",user="logan",passwd="ROFLdb!789",db="trashnet_db")
 x = conn.cursor()
-
+time = datetime.datetime.now()
+id = 1
+status = "full"
 try:
-   x.execute("INSERT INTO binStatus (Id, Status) VALUES (1,'Full')")
+   x.execute("INSERT INTO eventsLog (UnitId, EventType, EventTime) VALUES (%s, %s, %s)", (id, status, time))
+   x.execute("UPDATE binStatus SET Status ='Full' WHERE UnitId = " + id)
    conn.commit()
 except:
    conn.rollback()
